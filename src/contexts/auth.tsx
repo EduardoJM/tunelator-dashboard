201b4801bc2765fd84b2944c0a1418f21ba5acdc
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-04-13 20:07:44 -0300

    add check stored credentials

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,66 +1,103 @@
-import { FC, createContext, useState, useContext } from 'react';
-import { useToast } from '@chakra-ui/react';
-import { getErrorMessages } from '../utils/errors';
-import { loginSchema } from '../schemas/auth';
-import { User } from '../entities/User';
-import api from '../services/api';
-import * as authServices from '../services/auth';
+import { FC, createContext, useState, useContext, useEffect } from "react";
+import { useToast } from "@chakra-ui/react";
+import { getErrorMessages } from "../utils/errors";
+import { loginSchema } from "../schemas/auth";
+import { User } from "../entities/User";
+import api from "../services/api";
+import * as authServices from "../services/auth";
 
 export interface AuthContextData {
-    loggedIn: boolean;
-    userData: User | null;
-    login: (email: string, password: string, remember: boolean) => Promise<void>;
+  loggedIn: boolean;
+  userData: User | null;
+  login: (email: string, password: string, remember: boolean) => Promise<void>;
 }
 
-export const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+export const AuthContext = createContext<AuthContextData>(
+  {} as AuthContextData
+);
 
 export const AuthProvider: FC = ({ children }) => {
-    const toast = useToast();
-    const [userData, setUserData] = useState<User | null>(null);
-    
-    async function login(email: string, password: string, remember: boolean) {
-        try {
-            const validatedData = loginSchema.validateSync({ email, password, remember });
-            const response = await authServices.login(validatedData.email, validatedData.password);
-            
-            setUserData(response.user);
-            api.defaults.headers.common['Authorization'] = `Bearer ${response.access}`;
-            if (remember) {
-                localStorage.setItem('@TUNELATOR_REFRESH', response.refresh);
-            } else {
-                sessionStorage.setItem('@TUNELATOR_REFRESH', response.refresh);
-            }
-            toast({
-                title: 'Sucesso',
-                description: 'Login efetuado com sucesso!',
-                status: 'success',
-                duration: 5000,
-                isClosable: true,
-            });
-        } catch (err) {
-            getErrorMessages(err).forEach((error) => {
-                toast({
-                    title: error.title,
-                    description: error.text,
-                    status: 'error',
-                    duration: 5000,
-                    isClosable: true,
-                });
-            });
-        }
+  const toast = useToast();
+  const [userData, setUserData] = useState<User | null>(null);
+
+  useEffect(() => {
+    async function checkStoredCredentials() {
+      let token: string | null = null;
+      if (localStorage.getItem("@TUNELATOR_REFRESH")) {
+        token = localStorage.getItem("@TUNELATOR_REFRESH");
+      } else if (sessionStorage.getItem("@TUNELATOR_REFRESH")) {
+        token = localStorage.getItem("@TUNELATOR_REFRESH");
+      }
+      if (!token) {
+        return;
+      }
+      try {
+        const response = await authServices.refresh(token);
+        setUserData(response.user);
+        api.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${response.access}`;
+      } catch {
+        localStorage.removeItem("@TUNELATOR_REFRESH");
+        sessionStorage.removeItem("@TUNELATOR_REFRESH");
+      }
     }
 
-    return (
-        <AuthContext.Provider
-            value={{
-                loggedIn: !!userData,
-                userData,
-                login,
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
-    )
+    checkStoredCredentials();
+  }, []);
+
+  async function login(email: string, password: string, remember: boolean) {
+    try {
+      const validatedData = loginSchema.validateSync({
+        email,
+        password,
+        remember,
+      });
+      const response = await authServices.login(
+        validatedData.email,
+        validatedData.password
+      );
+
+      setUserData(response.user);
+      api.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${response.access}`;
+      if (remember) {
+        localStorage.setItem("@TUNELATOR_REFRESH", response.refresh);
+      } else {
+        sessionStorage.setItem("@TUNELATOR_REFRESH", response.refresh);
+      }
+      toast({
+        title: "Sucesso",
+        description: "Login efetuado com sucesso!",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+    } catch (err) {
+      getErrorMessages(err).forEach((error) => {
+        toast({
+          title: error.title,
+          description: error.text,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      });
+    }
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        loggedIn: !!userData,
+        userData,
+        login,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => useContext(AuthContext);
