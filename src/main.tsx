a960cc420355af1367aa38892d261257ccff84ee
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-04-18 11:04:54 -0300

    fix lazy routes problem

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { AuthProvider } from "./contexts/auth";
-import { LoadingProvider } from "./contexts/loading";
-import theme from "./theme";
-import AppRoutes from "./routes";
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { AuthProvider } from './contexts/auth';
+import { LoadingProvider } from './contexts/loading';
+import theme from './theme';
+import AppRoutes from './routes';
 
 const queryClient = new QueryClient();
 
@@ -17,12 +17,14 @@ ReactDOM.render(
         <AuthProvider>
           <BrowserRouter>
             <QueryClientProvider client={queryClient}>
-              <AppRoutes />
+              <Suspense fallback={<></>}>
+                <AppRoutes />
+              </Suspense>
             </QueryClientProvider>
           </BrowserRouter>
         </AuthProvider>
       </LoadingProvider>
     </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
