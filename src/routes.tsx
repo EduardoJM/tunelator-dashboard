9aba7dc9ce24c9adc3d0e3c3925fb3e4192a76d2
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-04-18 10:55:44 -0300

    configure chunks

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,18 @@
-import { FC } from 'react';
+import { FC, lazy } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './contexts/auth';
 import Dashboard from './layouts/Dashboard';
-import LoginPage from './pages/Login';
-import SignupPage from './pages/Signup';
-import HomePage from './pages/Home';
-import NotFoundPage from './pages/NotFound';
-import MailAccountsPage from './pages/MailAccounts';
+//import LoginPage from './pages/Login';
+//import SignupPage from './pages/Signup';
+//import Home from './pages/Home';
+//import NotFoundPage from './pages/NotFound';
+//import MailAccountsPage from './pages/MailAccounts';
+
+const LoginPage = lazy(() => import('./pages/Login'));
+const SignupPage = lazy(() => import('./pages/Signup'));
+const HomePage = lazy(() => import('./pages/Home'));
+const NotFoundPage = lazy(() => import('./pages/NotFound'));
+const MailAccountsPage = lazy(() => import('./pages/MailAccounts'));
 
 const AppRoutes: FC = () => {
   const { loggedIn } = useAuth();
