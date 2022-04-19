d4e7b3746fcc54395ea5a2f29c6f71bf6c32fa70
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-04-19 20:39:32 -0300

    add theming preparation for dark theme in the future

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,30 +1,35 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react';
 
 const colors = {
-    brand: {
-        50: '#a590e0',
-        100: '#8d6fe0',
-        200: '#8260e0',
-        300: '#6e45e0',
-        400: '#6639e2',
-        500: '#5F30E2',
-        600: '#4c26b5',
-        700: '#4925ad',
-        800: '#42219c',
-        900: '#311974'
-    },
-    foreground: {
-        default: '#000000',
-        muted: '#666666'
-    }
-}
+  brand: {
+    50: '#a590e0',
+    100: '#8d6fe0',
+    200: '#8260e0',
+    300: '#6e45e0',
+    400: '#6639e2',
+    500: '#5F30E2',
+    600: '#4c26b5',
+    700: '#4925ad',
+    800: '#42219c',
+    900: '#311974',
+  },
+  foreground: {
+    default: '#000000',
+    muted: '#666666',
+  },
+};
 
 const styles = {
-    global: {
-        fontFamily: '\'Roboto\', sans-serif',
-    }
-} 
+  global: {
+    fontFamily: "'Roboto', sans-serif",
+  },
+};
 
-  const theme = extendTheme({ colors, styles })
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+};
 
-  export default theme;
+const theme = extendTheme({ colors, styles, config });
+
+export default theme;
