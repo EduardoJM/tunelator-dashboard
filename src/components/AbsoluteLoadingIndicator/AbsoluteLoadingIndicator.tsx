fa9281f22da86d198b7b8bb5e1585136d8206142
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-04-27 20:39:18 -0300

    feat: add custom loading icon to absolute screen

--- src/components/AbsoluteLoadingIndicator/AbsoluteLoadingIndicator.tsx
+++ src/components/AbsoluteLoadingIndicator/AbsoluteLoadingIndicator.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
-import { Flex, CircularProgress } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import { useLoading } from '../../contexts/loading';
+import styles from './AbsoluteLoadingIndicator.module.scss';
 
 export interface AbsoluteLoadingIndicatorProps {
   force?: boolean;
@@ -22,11 +23,31 @@ const AbsoluteLoadingIndicator: FC<AbsoluteLoadingIndicatorProps> = ({
       left="0"
       top="0"
       backgroundColor="brand.500"
+      flexDirection="column"
       alignItems="center"
       justifyContent="center"
       zIndex="600"
     >
-      <CircularProgress isIndeterminate color="brand.50" />
+      <svg
+        className={styles.loadingAnimation}
+        width="512"
+        height="512"
+        viewBox="0 0 512 512"
+      >
+        <g>
+          <path
+            className={styles.outside}
+            d="M 88.857136,105.89839 H 423.14286 c 20.57715,0 37.14286,16.56572 37.14286,37.14286 v 237.85714 c 0,20.57714 -16.56571,37.14286 -37.14286,37.14286 H 88.857136 c -20.577143,0 -37.142857,-16.56572 -37.142857,-37.14286 V 143.04125 c 0,-20.57714 16.565714,-37.14286 37.142857,-37.14286 z"
+          />
+          <path
+            d="M 53.013939,151.8465 255.99358,254.7321 457.55248,151.12635"
+            className={styles.lines}
+          />
+        </g>
+      </svg>
+      <Text color="#FFF" mt="20px" fontSize="lg">
+        Carregando...
+      </Text>
     </Flex>
   );
 };
