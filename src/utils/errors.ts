0ef5742e1cdd794627332e05779e61b00a44a751
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-05-05 20:07:29 -0300

    fix: fix problem crashing page with wrong response error

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,43 +1,51 @@
 import { ValidationError } from 'yup';
 
 export interface Error {
-    title: string;
-    text: string;
+  title: string;
+  text: string;
 }
 
 const fieldsMapping = {
-    "email": "E-mail",
-    "password": "Senha"
-}
+  email: 'E-mail',
+  password: 'Senha',
+};
 
-export function getErrorMessages(err: any) : Error[] {
-    if (err instanceof ValidationError) {
-        return err.errors.map((text) => ({ title: 'Oopps!', text }));
-    } else if (!!err.response) {
-        const data = err.response.data as any;
-        if (Object.prototype.hasOwnProperty.call(data, 'detail')) {
-            return [
-                {
-                    title: 'Oopps!',
-                    text: String(data.detail),
-                },
-            ];
-        }
-        const keys = Object.keys(data).map((key) => {
-            const title = Object.prototype.hasOwnProperty.call(fieldsMapping, key)
-                ? String((fieldsMapping as any)[key])
-                : key;
-            const text = String(data[key]);
-            return { title, text };
-        });
-        if (keys.length > 0) {
-            return keys;
-        }
-    }
-    return [
+export function getErrorMessages(err: any): Error[] {
+  if (err instanceof ValidationError) {
+    return err.errors.map(text => ({ title: 'Oopps!', text }));
+  } else if (!!err.response) {
+    if (typeof err.response.data === 'string') {
+      return [
         {
-            title: 'Oopps!',
-            text: 'Não foi possível completar a operação.',
+          title: 'Oopps!',
+          text: 'Não foi possível completar a operação.',
         },
-    ];
+      ];
+    }
+    const data = err.response.data as any;
+    if (Object.prototype.hasOwnProperty.call(data, 'detail')) {
+      return [
+        {
+          title: 'Oopps!',
+          text: String(data.detail),
+        },
+      ];
+    }
+    const keys = Object.keys(data).map(key => {
+      const title = Object.prototype.hasOwnProperty.call(fieldsMapping, key)
+        ? String((fieldsMapping as any)[key])
+        : key;
+      const text = String(data[key]);
+      return { title, text };
+    });
+    if (keys.length > 0) {
+      return keys;
+    }
+  }
+  return [
+    {
+      title: 'Oopps!',
+      text: 'Não foi possível completar a operação.',
+    },
+  ];
 }
