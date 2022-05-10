6db69f5a0f277121ad29aba576a0837707968228
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-05-09 21:59:29 -0300

    test: add tests for Customer/Error page

---
+++ src/pages/Customer/Error/Error.spec.tsx
@@ -0,0 +1,49 @@
+import { ReactElement } from 'react';
+import { render as renderDefault, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+import Error from './Error';
+
+const render = (el: ReactElement) =>
+  renderDefault(el, { wrapper: BrowserRouter });
+
+describe('Success', () => {
+  it('must contains one button to plans in the page', () => {
+    render(<Error />);
+
+    const plansButton = screen.getByText(/^Planos e Assinatura$/i);
+
+    expect(plansButton).toBeInTheDocument();
+    expect(plansButton.tagName.toUpperCase()).toEqual('BUTTON');
+  });
+
+  it('must contain informations about payment error on the page', () => {
+    render(<Error />);
+
+    const heading = screen.getByText(
+      /^Houve um erro ao tentar te encaminhar para o portal do cliente na Stripe...$/i
+    );
+    const body = screen.getByText(
+      /^Tente novamente mais tarde e caso o erro persista, entre em contato com o suporte.$/i
+    );
+
+    expect(heading).toBeInTheDocument();
+    expect(body).toBeInTheDocument();
+  });
+
+  it('click on the plans button must navigate to plans', async () => {
+    render(<Error />);
+
+    const plansButton = screen.getByText(/^Planos e Assinatura$/i);
+
+    window.history.replaceState({}, '', '/any-route');
+
+    expect(window.location.pathname).toEqual('/any-route');
+
+    await act(async () => {
+      await userEvent.click(plansButton);
+    });
+
+    expect(window.location.pathname).toEqual('/plans');
+  });
+});
