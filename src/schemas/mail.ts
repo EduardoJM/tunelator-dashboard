81e06b36999c0bf6cee75008560e1611ba10de35
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-05-11 22:05:54 -0300

    refactor: remove some unused code from schemas

--- src/schemas/mail.ts
+++
@@ -1,9 +0,0 @@
-import { object, string, boolean } from 'yup';
-
-export const createOrEditMailSchema = object({
-  name: string().required('Insira um nome válido.'),
-  mail_user: string().required(),
-  redirect_enabled: boolean().required(),
-  redirect_to_my_email: boolean().required(),
-  redirect_to: string().optional().email('Insira um e-mail válido'),
-});
