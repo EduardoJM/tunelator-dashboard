291d6324fdc1b0b5eacbd189e070f094166567d3
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-08-27 21:36:54 -0300

    revert: revert delay into social content api route

--- src/services/api/socialContent.ts
+++ src/services/api/socialContent.ts
@@ -5,8 +5,6 @@ import {
 import api from './axios';
 
 export async function getLatestSocialContents(): Promise<SocialContent[]> {
-  await new Promise(resolve => setTimeout(resolve, 5000));
-
   const response = await api.get<SocialContentPaginatedResponse>(
     '/content/?limit=8'
   );
