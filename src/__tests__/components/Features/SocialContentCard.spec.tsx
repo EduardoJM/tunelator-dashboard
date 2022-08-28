190ec93ca7af9bea8acc82e376aac3da52186e48
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-08-27 21:53:02 -0300

    test: add basic unit test for Social Content Card

---
+++ src/__tests__/components/Features/SocialContentCard.spec.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import { SocialContentCard } from '@/components/Features';
+import socialContentFactory from '@/__mocks__/factories/socialContent';
+
+describe('SocialContentCard', () => {
+  it('should render the content type, title and description', () => {
+    const content = socialContentFactory();
+
+    render(<SocialContentCard content={content} height={300} />);
+
+    const title = screen.queryByText(content.title);
+    const type = screen.queryByText(content.type);
+    const desc = screen.queryByText(content.description || '');
+
+    expect(title).toBeInTheDocument();
+    expect(type).toBeInTheDocument();
+    expect(desc).toBeInTheDocument();
+  });
+
+  it('should render background image if image is parsed', () => {
+    const content = socialContentFactory({
+      image: 'any',
+    });
+
+    render(<SocialContentCard content={content} height={300} />);
+
+    const card = screen.queryByTestId('social-content-card');
+    expect(card).toHaveStyle('background-image: url(any)');
+  });
+
+  it('should contains an link to the content', async () => {
+    const content = socialContentFactory();
+
+    render(<SocialContentCard content={content} height={300} />);
+
+    const link = screen.queryByRole('link');
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute('href', content.link);
+  });
+});
